49caa68a54cff96feb2ccacb300c9225c0bb75b9
JoLo8570-oss <JoLo8570@student.harmonytx.org>
2026-01-22 13:54:43 -0600

    added new movies like fnaf

--- script.js
+++ script.js
@@ -51,6 +51,31 @@ const movies = [
     genre: "draman",
     poster: "images/the great gatsby.jpg"
   },
+  {
+    id: 6,
+    title: "Someone like you",
+    description: "Dawson searches for his hb sister.",
+    year: 2024,
+    genre: "romance",
+    poster: "images/someonelikeyou.jpg"
+  },
+  {
+    id: 7,
+    title: "Five Nights at Freddys",
+    description: "security gaurd tries to survive.",
+    year: 2023,
+    genre: "mystery",
+    poster: "images/fivenightsatfreddys.jpg"
+  },
+  {
+    id: 8,
+    title: "Halloween",
+    description: "Michael myers tries to unalive people.",
+    year: 1978,
+    genre: "horror",
+    poster: "images/halloween.jpg"
+  }
+
 ];
 
 
