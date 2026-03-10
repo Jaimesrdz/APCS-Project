3f4c49400d0fa3d4b58fabdaba67a1fec463a618
Jaime Rodriguez <jaimesrdz17@gmail.com>
2026-03-10 14:07:09 -0500

    Update movie.js

--- movie.js
+++ movie.js
@@ -5,13 +5,11 @@ const movieId = parseInt(params.get("id"));
 // Find the matching movie
 const movie = movies.find(m => m.id === movieId);
 
-// Display the movie
 if (movie) {
   document.getElementById("title").textContent = movie.title;
   document.getElementById("description").textContent = movie.description;
   document.getElementById("poster").src = movie.poster;
   document.getElementById("genre").textContent = movie.genre;
-  //document.getElementById("tags").textContent = movie.tags;
 } else {
   document.body.innerHTML = "<h1>Movie not found</h1>";
 }
